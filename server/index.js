5cbc8ea66cac906443afd1a3fc563ffcfa47c55c
Ishan Kumar <itsik159@gmail.com>
2021-06-26 15:57:10 +0530

    api pushed

--- server/index.js
+++ server/index.js
@@ -33,4 +33,4 @@ io.on("connection", (socket) => {
 	});
 });
 
-server.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+server.listen(PORT, () => console.log(`Api is running on port ${PORT}`));
