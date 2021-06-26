a94e39353cfeb6177d1f4d2412697c206fc16b3a
Ishan Kumar <itsik159@gmail.com>
2021-06-26 15:50:38 +0530

    base code

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+import VideoPlayer from './components/VideoPlayer';
+import Sidebar from './components/Sidebar';
+import Notifications from './components/Notifications';
+function App() {
+  return (
+    <div className="App">
+       <h1>Video Chat </h1>
+       <VideoPlayer />
+       <Sidebar>
+        <Notifications />
+      </Sidebar>
+    </div>
+  );
+}
+
+export default App;
